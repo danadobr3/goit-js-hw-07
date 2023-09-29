2f32a162a902a1ae491c5ac0783e45ac2b37416d
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-09-29 17:28:47 +0200

    task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const elementsList = document.querySelector('.gallery');
+
+elementsList.insertAdjacentHTML("beforeend", createMarkp(galleryItems))
+
+function createMarkp(arr) {
+     
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+}
